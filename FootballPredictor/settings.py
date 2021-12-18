b72c93fd0f2807e223e8d68842172589a6caad24
Grahev <mumin20@gmail.com>
2021-12-18 23:41:59 +0000

    env var 2

--- FootballPredictor/settings.py
+++ FootballPredictor/settings.py
@@ -169,8 +169,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'assafootball2021@gmail.com'
-EMAIL_HOST_USER = S3Connection(os.environ['EMAIL_HOST_USER'])
-EMAIL_HOST_PASSWORD = S3Connection(os.environ['EMAIL_HOST_PASSWORD'])
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER','dev default value')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD','dev default value')
 EMAIL_PORT = 587
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
