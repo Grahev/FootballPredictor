d3e2fe470ba08da6e9329fcf50d3956997336929
Grahev <mumin20@gmail.com>
2022-02-10 22:45:06 +0000

    admin change

--- predictor/admin.py
+++ predictor/admin.py
@@ -3,9 +3,13 @@ from .models import Team, Player, Match, MatchPrediction, MatchEvents,League
 
 # Register your models here.
 
+class RatingAdmin(admin.ModelAdmin):
+    readonly_fields = ('date',)
+
+
 admin.site.register(Team)
 admin.site.register(Player)
 admin.site.register(Match)
 admin.site.register(MatchPrediction)
 admin.site.register(MatchEvents)
-admin.site.register(League)
+admin.site.register(League,RatingAdmin)
